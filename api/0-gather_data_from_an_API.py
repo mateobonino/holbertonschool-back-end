baf6c82ae042b236360a69c7568b96f84f1e6fb6
Mateo Bonino <mateo@localhost.localdomain>
2022-11-28 05:20:59 -0300

    tired of documentation

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -16,22 +16,21 @@ if __name__ == '__main__':
     user_response = requests.get(
         get_name,
         params={'id': int(user_id)})
-    count = 0
-    total_tasks = 0
+    NUMBER_OF_DONE_TASKS = 0
+    TOTAL_NUMBER_OF_TASKS = 0
     tasks_text = []
 
     for key in response.json():
         if key['completed'] is True:
-            count += 1
+            NUMBER_OF_DONE_TASKS += 1
             tasks_text.append(key['title'])
-        total_tasks += 1
-    employee_name = ''
+        TOTAL_NUMBER_OF_TASKS += 1
+    EMPLOYEE_NAME = ''
     for name in user_response.json():
         if 'name' in name.keys():
             employee_name = name['name']
 
-    print("Employee {} is done with tasks({}/{}):".format(employee_name,
-                                                          count,
-                                                          total_tasks))
-    for task in tasks_text:
-        print("\t {}".format(task))
+    to_print = f"Employee {EMPLOYEE_NAME} is done with tasks"
+    to_print += f"({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS})"
+    for TASK_TITLE in tasks_text:
+        print("\t {}".format(TASK_TITLE))
