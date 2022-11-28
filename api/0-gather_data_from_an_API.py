b2980c087ad3c675ffcdac90e58b5c01a06f5171
Mateo Bonino <mateo@localhost.localdomain>
2022-11-28 05:07:34 -0300

    Task 2 documentatioooooon

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -9,35 +9,32 @@ import requests
 import sys
 
 
-def request_api():
-    """
-    Makes a request to the JSONPlaceholder API
-    """
-    user_id = sys.argv[1]
-    url = f"https://jsonplaceholder.typicode.com/todos?userId={user_id}"
-    get_name = f"https://jsonplaceholder.typicode.com/users?id={user_id}"
-    response = requests.get(url)
-    user_response = requests.get(get_name)
-    count = 0
-    total_tasks = 0
-    tasks_text = []
-
-    for key in response.json():
-        if key['completed'] is True:
-            count += 1
-            tasks_text.append(key['title'])
-        total_tasks += 1
-    employee_name = ''
-    for name in user_response.json():
-        if 'name' in name.keys():
-            employee_name = name['name']
-
-    print("Employee {} is done with tasks({}/{})):".format(employee_name,
-                                                           count,
-                                                           total_tasks))
-    for task in tasks_text:
-        print("\t {}".format(task))
+"""
+Makes a request to the JSONPlaceholder API
+"""
 
 
-if __name__ == '__main__':
-    request_api()
+user_id = sys.argv[1]
+url = f"https://jsonplaceholder.typicode.com/todos?userId={user_id}"
+get_name = f"https://jsonplaceholder.typicode.com/users?id={user_id}"
+response = requests.get(url)
+user_response = requests.get(get_name)
+count = 0
+total_tasks = 0
+tasks_text = []
+
+for key in response.json():
+    if key['completed'] is True:
+        count += 1
+        tasks_text.append(key['title'])
+    total_tasks += 1
+employee_name = ''
+for name in user_response.json():
+    if 'name' in name.keys():
+        employee_name = name['name']
+
+print("Employee {} is done with tasks({}/{})):".format(employee_name,
+                                                       count,
+                                                       total_tasks))
+for task in tasks_text:
+    print("\t {}".format(task))
