7651e67e01a68e8d1ce9c30446a91d370af041f5
Mateo Bonino <mateo@localhost.localdomain>
2022-11-28 04:52:10 -0300

    Task 0 Fixed doc

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,28 +1,33 @@
 #!/usr/bin/python3
-import requests
-import json
-import sys
 """
 For a given employee ID, returns information about his/her TODO list progress.
 """
+import requests
+import json
+import sys
+
+
 user_id = sys.argv[1]
 url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(user_id)
 get_name = "https://jsonplaceholder.typicode.com/users?id={}".format(user_id)
 response = requests.get(url)
 user_response = requests.get(get_name)
-user_result = user_response.json()
 result = response.json()
 count = 0
+total_tasks = 0
 tasks_text = []
-for key in result:
+
+for key in response.json():
     if key['completed'] is True:
         count += 1
         tasks_text.append(key['title'])
+    total_tasks += 1
 employee_name = ''
-for name in user_result:
+for name in user_response.json():
     if 'name' in name.keys():
         employee_name = name['name']
 
-print("Employee {} is done with tasks({}/20):".format(employee_name, count))
+print("Employee {} is done with tasks({}/{})):".format(employee_name, count,
+                                                       total_tasks))
 for task in tasks_text:
     print("     {}".format(task))
