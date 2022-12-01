c602d7376a0a71f1f75dde7046907ae94176ad51
Mateo Bonino <mbonino1810@gmail.com>
2022-12-01 10:32:12 -0300

    Task 1

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -20,18 +20,11 @@ if __name__ == '__main__':
     total_tasks = 0
     tasks_text = []
 
-    for key in response.json():
-        if key['completed'] is True:
-            count += 1
-            tasks_text.append(key['title'])
-        total_tasks += 1
-    employee_name = ''
     for name in user_response.json():
         if 'name' in name.keys():
             employee_name = name['name']
 
-    print("Employee {} is done with tasks({}/{}):".format(employee_name,
-                                                          count,
-                                                          total_tasks))
-    for task in tasks_text:
-        print("\t {}".format(task))
+    for key in response.json():
+        print('"{}", "{}", "{}", "{}"'.format(user_id,
+                                              employee_name, key['completed'],
+                                              key['title']))
