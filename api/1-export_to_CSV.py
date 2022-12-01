2625af896273a1e2c0554aba9c2ade54bd17bbdb
Mateo Bonino <mbonino1810@gmail.com>
2022-12-01 10:55:40 -0300

    Task 1 fix

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -9,21 +9,19 @@ import sys
 if __name__ == '__main__':
     user_id = sys.argv[1]
     url = "https://jsonplaceholder.typicode.com/todos"
-    get_name = "https://jsonplaceholder.typicode.com/users"
+    get_name = "https://jsonplaceholder.typicode.com/users/{}".format(user_id)
     response = requests.get(
         url,
         params={'userId': int(user_id)})
-    user_response = requests.get(
-        get_name,
-        params={'id': int(user_id)})
-
-    for name in user_response.json():
-        if 'name' in name.keys():
-            employee_name = name['name']
+    user_response = requests.get(get_name).json()
+    employee_name = ''
+    if user_response['id'] == int(user_id):
+        employee_name = user_response['username']
 
     for key in response.json():
         with open('{}.csv'.format(sys.argv[1]), 'a+') as f:
-            f.write('"{}", "{}", "{}", "{}"\n'.format(user_id,
-                                                    employee_name, key['completed'],
-                                                    key['title']))
+            f.write('"{}","{}","{}","{}"\n'.format(user_id,
+                                                   employee_name,
+                                                   key['completed'],
+                                                   key['title']))
     f.close()
